5f7741432dbad128120229cc172d40aa90d954ab
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-29 15:27:46 +0530

    loops

---
+++ mongodb.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+spinner() {
+    local pid=$1
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    # Clear spinner characters after loop ends
+    printf "      \b\b\b\b\b\b"
+}
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
+
+mkdir -p $LOGS_FOLDER
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR: Please run this script with root privileges"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2...$R{Failed}$N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2...$G{Success}$N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf list installed mongodb-org &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    echo -ne "$Y{Installing}$N MongoDB"
+    dnf install -y mongodb-org &>>$LOG_FILE &   
+    pid=$!
+    spinner $pid
+    wait $pid
+    printf "\r\033[K"
+    VALIDATE $? "MongoDB Installation"
+else 
+    echo -e "MongoDB already exists$Y SKIPPING$N installation of MongoDB" | tee -a $LOG_FILE
+fi
+
+systemctl enable mongod
+VALIDATE $? "Enabling MongoDB Service"
+
+systemctl start mongod
+VALIDATE $? "Starting MongoDB Service" 
