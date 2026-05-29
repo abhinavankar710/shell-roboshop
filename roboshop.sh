491ae783af9ee4771c9fc4fa9c9f242cc6f758ff
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-05-29 14:49:29 +0530

    loops

--- roboshop.sh
+++ roboshop.sh
@@ -2,17 +2,29 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0668b48020a3df8ef"
+ZONE_ID="Z031697215LI2TC1B66AG"
+DOMAIN_NAME="ankar.space"
 
-for instance in $@
+for instance in "$@"
 do
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0668b48020a3df8ef --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI_ID" --instance-type t3.micro --security-group-ids "$SG_ID" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="$instance"}]" --query 'Instances[0].InstanceId' --output text)
+
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
 
     # Get Private IP of the instance.
-    if [ $instance != "frontend" ]; then
-        PRIVATE_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        echo "Private IP of the Instance $instance is: $PRIVATE_IP"
+    if [ "$instance" != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        echo "Private IP of the Instance "$instance" is: "$IP""
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+        IP_TYPE="(Private)"
+
     else
-        PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        echo "Public IP of the Instance $instance is: $PUBLIC_IP"
+        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        echo "Public IP of the Instance "$instance" is: "$IP""
+        RECORD_NAME="$DOMAIN_NAME"
+        IP_TYPE="(Public)"
     fi
+        # Update the DNS record in Route53
+        aws route53 change-resource-record-sets --hosted-zone-id "$ZONE_ID" --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'"$RECORD_NAME"'","Type":"A","TTL":1,"ResourceRecords":[{"Value":"'"$IP"'"}]}}]}'
+        echo "Updating Route53 Record: "$RECORD_NAME" → For The Instance "$instance":"$IP $IP_TYPE""
 done
