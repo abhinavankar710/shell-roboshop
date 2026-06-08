f7438c82dc88c5b284115c94821a84581e0c1ca4
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-06-08 12:59:06 +0530

    MongoDB

--- mongodb.sh
+++ mongodb.sh
@@ -24,7 +24,7 @@ LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
 
-mkdir -p $LOGS_FOLDER &>>$LOG_FILE
+mkdir -p $LOGS_FOLDER &>>/dev/null
 echo "Script execution started at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
