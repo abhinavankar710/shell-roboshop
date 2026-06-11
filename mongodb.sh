304b2aaa3393c3be5ecde42d66cc3fd4a8821681
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-06-11 14:40:32 +0530

    Roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -72,7 +72,7 @@ systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling MongoDB Service"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Launching MongoDB Service"
+VALIDATE $? "Executing MongoDB Service"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing Remote Connections to MongoDB"
