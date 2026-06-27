b35f54fce76cdd8f8d7f4b1b329cdd37fa3ec94e
abhinavankar710 <abhinav.ankar710@gmail.com>
2026-06-27 16:50:52 +0530

    Roboshop

---
+++ 20-script-1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+NAME=INDIA
+echo "My Country Name: $NAME"
+echo "PID of script-1: $$"
+sh 21-script-2.sh
---
+++ 21-script-2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "My Country Name: $NAME"
+echo "PID of script-2: $$"
